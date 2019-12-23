50a5599f2b0774d1f5ebb794f5a2fe8e0ebbdf00
raychanid <46307150+raychanid@users.noreply.github.com>
2019-12-23 11:15:40 +0800

    Create hw.php

---
+++ hw.php
@@ -0,0 +1,55 @@
+PHP 教程
+
+PHP 是一种创建动态交互性站点的强有力的服务器端脚本语言。
+
+PHP 是免费的，并且使用非常广泛。同时，对于像微软 ASP 这样的竞争者来说，PHP 无疑是另一种高效率的选项。
+
+适用于PHP初学者的学习线路和建议
+
+PHP 开发工具推荐
+
+PHP 在线工具
+
+通过实例学习 PHP
+我们的 PHP 在线实例让您能够更简单的学习 PHP，实例中包含了 PHP 的源码及运行结果。
+
+实例
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "Hello World!";
+?>
+
+</body>
+</html>
+
+运行实例 »
+点击"运行实例"按钮查看在线实例运行结果。
+
+
+PHP 入门视频教程
+
+PHP 参考手册
+在菜鸟教程中，您会发现所有 PHP 函数的完整参考手册：
+
+Array 函数
+Calendar 函数
+cURL 函数
+Date 函数
+Directory 函数
+Error 函数
+Filesystem 函数
+Filter 函数
+FTP 函数
+HTTP 函数
+LibXML 函数
+Mail 函数
+Math 函数
+Misc 函数
+MySQLi 函数
+SimpleXML 函数
+String 函数
+XML Parser 函数
+Zip 函数
